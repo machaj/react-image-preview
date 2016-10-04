02b57048b6d0a7ac6397554f56332a86c7576c90
Jan Macháček <jan.machacek@topmonks.com>
2016-10-04 16:46:57 +0200

    Small changes

--- demo/example.js
+++ demo/example.js
@@ -2,12 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ImagePreviewHolder from '../lib/ImagePreviewHolder';
 
-var Example = React.createClass({
-    render: function() {
-        return (
-            <ImagePreviewHolder />
-        );
-    }
-});
-
-ReactDOM.render(<Example />, document.getElementById("container"));
+ReactDOM.render(<ImagePreviewHolder />, document.getElementById("container"));
